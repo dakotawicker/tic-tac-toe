e7d67f971d31cc86a4ea241f00dae8931d14b1b3
Dakota Wicker <51934913+dakotawicker@users.noreply.github.com>
2025-06-06 23:25:58 -0500

    Added Milestone Project
    Uploaded work done in VS Code as well as the readme.

---
+++ tic_tac_toe.py
@@ -0,0 +1,10 @@
+#Display Board
+def display_board(board):
+    print('\n')
+    print(f' {board[1]} | {board[2]} | {board[3]}')
+    print('---|---|---')
+    print(f' {board[4]} | {board[5]} | {board[6]}')
+    print('---|---|---')
+    print(f' {board[7]} | {board[8]} | {board[9]}')
+    print('\n')
+    
